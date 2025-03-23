31b829d061f16f173e1aa2fe20125db6a844b89a
Guillaume <guillaumexue@outlook.fr>
2025-03-23 17:05:01 +0100

    [Refactor] Enhance GUI with map viewer and path display functionality; remove unused paths.txt

--- src/main/java/fr/u_paris/gla/project/views/Gui.java
+++ src/main/java/fr/u_paris/gla/project/views/Gui.java
@@ -1,16 +1,18 @@
 package fr.u_paris.gla.project.views;
 
+import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
 import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
 
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import fr.u_paris.gla.project.graph.Stop;
 
 public class Gui extends JFrame {
 
@@ -19,10 +21,10 @@ public class Gui extends JFrame {
   private static final int RESEARCH_PANEL_WIDTH = 350;
   private static final int MIN_SCREEN_WIDTH = 600;
   private static final int MIN_SCREEN_HEIGHT = 300;
-  private static final String TXT_PATH_FILE = "/txt/paths.txt";
   private JTextArea textStart;
   private JTextArea textEnd;
   private JPanel contentPanel;
+  private JMapViewer mapViewer;
   private static final Color textColor = new Color(11, 22, 44);
   private static final Color bordeColor = new Color(88, 88, 88);
   private static final Color primaryBackgroundColor = new Color(240, 240, 240);
@@ -96,7 +98,10 @@ public class Gui extends JFrame {
     splitPane.add(scrollPane, JSplitPane.BOTTOM);
 
     // Create a map panel
-    JPanel mapPanel = createMapViewerPanel();
+    this.mapViewer = createMapViewer();
+    JPanel mapPanel = new JPanel();
+    mapPanel.setLayout(new java.awt.BorderLayout());
+    mapPanel.add(this.mapViewer, java.awt.BorderLayout.CENTER);
 
     // Add the split pane and the map panel to the main content panel
     JPanel mainContentPanel = new JPanel();
@@ -112,7 +117,7 @@ public class Gui extends JFrame {
    * 
    * @return the map viewer panel
    */
-  private static JPanel createMapViewerPanel() {
+  private static JMapViewer createMapViewer() {
     JMapViewer mapViewer = new JMapViewer();
     mapViewer.setTileSource(new OsmTileSource.Mapnik());
 
@@ -120,11 +125,7 @@ public class Gui extends JFrame {
     mapViewer.setDisplayPosition(
         new org.openstreetmap.gui.jmapviewer.Coordinate(48.8566, 2.3522), 10);
 
-    JPanel mapPanel = new JPanel();
-    mapPanel.setLayout(new java.awt.BorderLayout());
-    mapPanel.add(mapViewer, java.awt.BorderLayout.CENTER);
-
-    return mapPanel;
+    return mapViewer;
   }
 
   /**
@@ -192,7 +193,14 @@ public class Gui extends JFrame {
     // Add action listener to add displayJsonContent to contentPanel
     buttonSearch.addActionListener(e -> {
       contentPanel.removeAll();
-      contentPanel.add(displayTxtContent());
+      ArrayList<Stop> stops = new ArrayList<Stop>();// replace by the algorithm to find the path
+      stops.add(new Stop(48.37960363189276, 2.942541935520315, "Montereau"));
+      stops.add(new Stop(48.37863280606578, 2.896902458012003, "La Grande-Paroisse"));
+      stops.add(new Stop(48.38667549692297, 2.8421499951400837, "Vernou-sur-Seine"));
+      stops.add(new Stop(48.40726498958356, 2.798915755216375, "Champagne-sur-Seine"));
+      stops.add(new Stop(48.430461048527036, 2.752898337073854, "Vulaines-sur-Seine - Samoreau"));
+      stops.add(new Stop(48.44272124429534, 2.7607507689116346, "Héricy"));
+      contentPanel.add(displayPath(stops));
       contentPanel.revalidate();
       contentPanel.repaint();
     });
@@ -216,56 +224,35 @@ public class Gui extends JFrame {
    * 
    * @return the panel containing the text content
    */
-  private JPanel displayTxtContent() {
+  private JPanel displayPath(ArrayList<Stop> stops) {
     JPanel pathPanel = new JPanel();
     pathPanel.setLayout(new BoxLayout(pathPanel, BoxLayout.Y_AXIS));
     pathPanel.setBackground(primaryBackgroundColor); // Background color of paths panel
     pathPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Padding for the panel
 
-    try (InputStream inputStream = Gui.class.getResourceAsStream(TXT_PATH_FILE)) {
-      if (inputStream == null) {
-        // Handle the case where the file is not found
-        JLabel errorLabel = new JLabel("Error: File not found - " + TXT_PATH_FILE);
-        errorLabel.setForeground(Color.RED);
-        pathPanel.add(errorLabel);
-        return pathPanel;
-      }
-      InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-      StringBuilder content = new StringBuilder();
-      int c;
-      while ((c = reader.read()) != -1) {
-        if (c == '\n') {
-          String line = content.toString();
-          JTextArea lineTextArea = createTextArea(line);
-          lineTextArea.setEditable(false); // Make the text area read-only
-          lineTextArea.setFocusable(false); // Disable focus to prevent selection
-          if (line.matches("\\d+")) {
-            // If the line is a number, display it without additional formatting
-            lineTextArea.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-          }
-          pathPanel.add(lineTextArea);
-          content.setLength(0); // Clear the content for the next line
-        } else {
-          content.append((char) c);
-        }
-      }
-      // Add the last line if it exists
-      if (content.length() > 0) {
-        String line = content.toString();
-        JTextArea lineTextArea = createTextArea(line);
-        lineTextArea.setEditable(false);
-        lineTextArea.setFocusable(false); // Disable focus to prevent selection
-        if (line.matches("\\d+")) {
-          lineTextArea.setFont(new Font("Segoe UI", Font.BOLD, 16));
-          lineTextArea.setBackground(primaryBackgroundColor);
-        }
-        pathPanel.add(lineTextArea);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      JLabel errorLabel = new JLabel("Error reading file: " + e.getMessage());
-      errorLabel.setForeground(Color.RED);
-      pathPanel.add(errorLabel);
+    // initialize the first stop
+    Stop stop = stops.get(0);
+    JTextArea textArea = createTextArea(stop.getNameOfAssociatedStation());
+    pathPanel.add(textArea);
+    Coordinate mStart = new Coordinate(stop.getLongitude(), stop.getLatitude());
+    mapViewer.setDisplayPosition(mStart, 12);
+    MapMarkerDot parisMarker = new MapMarkerDot(mStart);
+    mapViewer.addMapMarker(parisMarker);
+
+    // draw the path and add TextAreas for each stop
+    for (int i = 1; i < stops.size(); i++) {
+      stop = stops.get(i);
+      // add TextArea for the stop
+      textArea = createTextArea(stop.getNameOfAssociatedStation());
+      pathPanel.add(textArea);
+      // draw the path
+      Coordinate mEnd = new Coordinate(stop.getLongitude(), stop.getLatitude()); // Paris center
+      MapPolygon mLine = new MapPolygonImpl(mStart, mEnd, mStart);
+      MapMarkerDot markerDot = new MapMarkerDot(mEnd);
+      mapViewer.addMapPolygon(mLine);
+      mapViewer.addMapMarker(markerDot);
+      // update the start
+      mStart = mEnd;
     }
     return pathPanel;
   }
