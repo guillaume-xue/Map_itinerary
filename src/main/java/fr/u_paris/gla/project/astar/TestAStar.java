07c7ca344bec26e38630b8eab1755f341b5283a6
micka <@thenot>
2025-03-24 14:55:49 +0100

    Mise à jour de TestAStar

--- src/main/java/fr/u_paris/gla/project/astar/TestAStar.java
+++ src/main/java/fr/u_paris/gla/project/astar/TestAStar.java
@@ -1,2 +1,25 @@
-package fr.u_paris.gla.project.astar;public class TestAStar {
+package fr.u_paris.gla.project.astar;
+
+import fr.u_paris.gla.project.graph.Graph;
+import fr.u_paris.gla.project.graph.Line;
+import fr.u_paris.gla.project.graph.Stop;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TestAStar {
+    @Test
+    public void AStarTest(){
+        //ArrayList<Subline> sublines = new ArrayList();
+        ArrayList<Stop> listOfStops = new ArrayList();
+        ArrayList<Line> listOfLines = new ArrayList();
+        Graph graph = new Graph(listOfStops, listOfLines);
+
+        AStar aStar = new AStar(graph);
+
+        Stop stop1 = new Stop(0F,0F, "Gare du Nord");
+        Stop stop2 = new Stop(0F,0F, "Bourg-la-Reine");
+
+        aStar.astar(graph, stop1, stop2);
+    }
 }
