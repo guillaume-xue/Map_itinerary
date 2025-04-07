4dbd4745acb4b62c6a395d56c3d99f616a731977
Guillaume <guillaumexue@outlook.fr>
2025-04-07 16:28:35 +0200

    Enhance MouseController to manage start and end coordinates, and update GUI to remove existing mouse listeners

--- src/main/java/fr/u_paris/gla/project/controllers/MouseController.java
+++ src/main/java/fr/u_paris/gla/project/controllers/MouseController.java
@@ -2,27 +2,69 @@ package fr.u_paris.gla.project.controllers;
 
 import java.awt.Point;
 
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseEvent;
 
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
 
 public class MouseController {
 
   private JMapViewer mapViewer;
   private Point lastDragPoint;
+  private JTextArea startTextArea;
+  private JTextArea endTextArea;
 
   /**
    * Constructor for MouseController.
    *
    * @param mapViewer the JMapViewer to control
    */
-  MouseController(JMapViewer mapViewer) {
+  MouseController(JMapViewer mapViewer, JTextArea startTextArea, JTextArea endTextArea) {
     this.mapViewer = mapViewer;
+    this.startTextArea = startTextArea;
+    this.endTextArea = endTextArea;
     this.lastDragPoint = new Point();
     addMouseController();
+  }
+
+  /**
+   * Adds markers to the map if startTextArea and endTextArea contain valid
+   * coordinates.
+   */
+  private void addInitialMarkers() {
+    try {
+      if (isValidCoordinate(startTextArea.getText())) {
+        String[] startCoords = startTextArea.getText().split(",\\s*");
+        Coordinate startCoord = new Coordinate(Double.parseDouble(startCoords[0]), Double.parseDouble(startCoords[1]));
+        mapViewer.addMapMarker(new MapMarkerDot(startCoord));
+      }
+      if (isValidCoordinate(endTextArea.getText())) {
+        String[] endCoords = endTextArea.getText().split(",\\s*");
+        Coordinate endCoord = new Coordinate(Double.parseDouble(endCoords[0]), Double.parseDouble(endCoords[1]));
+        mapViewer.addMapMarker(new MapMarkerDot(endCoord));
+      }
+    } catch (Exception e) {
+      System.err.println("Error parsing coordinates: " + e.getMessage());
+    }
+  }
+
+  /**
+   * Validates if a string represents valid coordinates in the format "latitude,
+   * longitude".
+   *
+   * @param text the string to validate
+   * @return true if valid, false otherwise
+   */
+  private boolean isValidCoordinate(String text) {
+    if (text == null || !text.matches("-?\\d+(\\.\\d+)?\\s*,\\s*-?\\d+(\\.\\d+)?")) {
+      return false;
+    }
+    return true;
   }
 
   /**
@@ -45,6 +87,23 @@ public class MouseController {
     @Override
     public void mouseReleased(MouseEvent e) {
       lastDragPoint = null;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+      if (e.getClickCount() == 2 && SwingUtilities.isLeftMouseButton(e)) {
+        mapViewer.removeAllMapMarkers();
+        Point clickPoint = e.getPoint();
+        Coordinate coord = new Coordinate(mapViewer.getPosition(clickPoint).getLat(),
+            mapViewer.getPosition(clickPoint).getLon());
+        mapViewer.addMapMarker(new MapMarkerDot(coord));
+        if (startTextArea.isFocusOwner()) {
+          startTextArea.setText(coord.getLat() + ", " + coord.getLon());
+        } else if (endTextArea.isFocusOwner()) {
+          endTextArea.setText(coord.getLat() + ", " + coord.getLon());
+        }
+        addInitialMarkers();
+      }
     }
   }
 
