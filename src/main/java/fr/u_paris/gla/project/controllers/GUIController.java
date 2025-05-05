52f158748c609527b0ddd064cba4c9c35f77a4e9
RICHARD guives <guives.richard@gmail.com>
2025-05-05 11:34:03 +0200

    Connecting stops from GUI

--- src/main/java/fr/u_paris/gla/project/controllers/GUIController.java
+++ src/main/java/fr/u_paris/gla/project/controllers/GUIController.java
@@ -35,6 +35,8 @@ public class GUIController {
   private Gui gui;
   private Graph graph;
 
+  private boolean areStopsConnected = false;
+
   /**
    * Constructor for GUIController.
    * Initializes the GUI and sets up event listeners.
@@ -392,11 +394,32 @@ public class GUIController {
         gui.cleanMap();
       }
     });
-    JMenuItem LineMenuItem = gui.getMenuItem(0, 3);
-    LineMenuItem.addActionListener(e -> {
-      gui.toggleCheckmark(LineMenuItem);
-      gui.toggleFloatingWindow(gui.isCheckmarkEnabled(LineMenuItem));
+    JMenuItem lineMenuItem = gui.getMenuItem(0, 3);
+    lineMenuItem.addActionListener(e -> {
+      gui.toggleCheckmark(lineMenuItem);
+      gui.toggleFloatingWindow(gui.isCheckmarkEnabled(lineMenuItem));
     });
+
+    JMenuItem connectStopsMenuItem = gui.getMenuItem(1,0);
+    connectStopsMenuItem.addActionListener(e -> {
+      if ( !areStopsConnected ){
+        int confirm = JOptionPane.showConfirmDialog(
+        this.gui, 
+        "Attention option expérimentale. Connecter les stations entre-elles pour les trajets à pied\n" + 
+        " prend du temps et rend l'algorithme de recherche plus lent. Si vous confirmez l'action\n" + 
+        " garder un oeil sur le terminal pour savoir quand les connections ont fini d'être créées.\n"
+        ,"Confirmation requise"
+        , JOptionPane.YES_NO_OPTION);
+        if ( confirm == 0 ){
+          graph.connectStopsByWalkingV2();
+          this.areStopsConnected = true;
+        } 
+      } else {
+        JOptionPane.showMessageDialog(this.gui, "Stations déjà connectées. Relancer l'application pour enlever cette option.",
+            "Info",
+            JOptionPane.ERROR_MESSAGE);
+      }
+    }); 
   }
 
   public void launch() {
